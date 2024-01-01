26ff6c2ac03e5543098c401580a8a080640a2fcd
Hadar Meron <meron.hadarr@gmail.com>
2024-01-01 23:41:30 +0200

    players movements works
    3 players can run / jump / fly
    next need to stick them together and activate both abilities

---
+++ 2024-Team-5/Assets/Hadar/Scripts/HadarGameManager.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HadarGameManager : MonoBehaviour
+{
+    public static HadarGameManager instance {private set; get;}
+    [SerializeField] private GameObject activePlayer;
+    [SerializeField] private GameObject[] players;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject GetActivePlayer()
+    {
+        return activePlayer;
+    }
+
+    private void Start()
+    {
+        activePlayer.GetComponent<PlayerMovment>().setMode(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SwitchPlayer();
+        }
+    }
+    
+    private void SwitchPlayer()
+    {
+        var index = Array.IndexOf(players, activePlayer);
+        Debug.Log($"index: {index}");
+        index++;
+        if (index >= players.Length) { index = 0; }
+        activePlayer = players[index];
+        foreach (var player in players)
+        {
+            var playerMovement = player.GetComponent<PlayerMovment>();
+            
+            if (player == activePlayer)
+            {
+                playerMovement.setMode(true);
+            }
+            else
+            {
+                playerMovement.setMode(false);
+
+            }
+        }
+    }
+}
--- 2024-Team-5/Assets/Hadar/Scripts/PlayerMovment.cs
+++ 2024-Team-5/Assets/Hadar/Scripts/PlayerMovment.cs
@@ -1,26 +1,87 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
-    private Rigidbody _rb;
-    private float _speed = 5f;  
-    private float _jumpForce = 5f;
-    
-    [SerializeField] private isGrounded isGrounded;
-    
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float jumpForce = 10.0f;
+    [SerializeField] private float flapForce = 5.0f;
+    [SerializeField] private float maxVelocity = 30f;
+    private float flyFlapTime = 2f;
+    [SerializeField] private float initialFlyFlapTime = 2f;
+    [SerializeField] private IsGrounded isGrounded;
+    [SerializeField] private bool canJump;
+    [SerializeField] private bool canFly;
+    [SerializeField] private bool canRun;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private Vector3 movedirection;
+    private Rigidbody objRb;
+    private bool _active;
+
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        objRb = GetComponent<Rigidbody>();
+        flyFlapTime = initialFlyFlapTime;
+    }
+
+    private void Update()
+    {
+        if (!_active)
+        {
+            movedirection = Vector3.zero;
+            return;
+        }
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        movedirection = new Vector3(horizontalInput , 0 ,verticalInput);
+
+        if (canJump)
+        {
+            Jump();
+        }
+        
+        if (canFly)
+        {
+            Fly();
+        }
+    }
+
+    private void Fly()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && flyFlapTime < 0)
+        {
+            objRb.AddForce(Vector3.up * flapForce, ForceMode.Impulse);
+            flyFlapTime = initialFlyFlapTime;
+        }
+        
+        if (flyFlapTime >= 0)
+        {
+            flyFlapTime -= Time.deltaTime;
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded.Grounded())
+        {
+            objRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        speed = canRun ? speed : 2f;
+        objRb.AddForce(movedirection * speed);
+        objRb.velocity = objRb.velocity.magnitude < maxVelocity ? objRb.velocity : objRb.velocity.normalized * maxVelocity;
     }
     
-    void Update()
+    public void setMode (bool active)
     {
-        _horizontalInput = Input.GetAxis("Horizontal");
-
-        if (!Input.GetKeyDown(KeyCode.Space) || !_isGrounded) return;
-        _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-        _isGrounded = false;
+        _active = active;
     }
 }
