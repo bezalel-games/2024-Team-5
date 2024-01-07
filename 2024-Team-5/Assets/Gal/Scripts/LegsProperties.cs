469e47b3778554ffc81b8317ef728370c22c9bc0
Gal <galgulz96@gmail.com>
2024-01-07 16:02:13 +0200

    creating hands&legs scriptable objects

---
+++ 2024-Team-5/Assets/Gal/Scripts/LegsProperties.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Gal.Scripts
+{
+    public class LegsProperties : ScriptableObject
+    {
+        public float speed;
+        public bool canJump;
+        public float jumpForce;
+        
+    }
+}
