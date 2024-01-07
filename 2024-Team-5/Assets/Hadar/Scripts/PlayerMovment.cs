4b2c5cd95e2e73fcb5f7931700943e027fa608e0
Hadar Meron <meron.hadarr@gmail.com>
2024-01-07 14:46:57 +0200

    changes to hadar scene

--- 2024-Team-5/Assets/Hadar/Scripts/PlayerMovment.cs
+++ 2024-Team-5/Assets/Hadar/Scripts/PlayerMovment.cs
@@ -9,18 +9,16 @@ public class PlayerMovment : MonoBehaviour
     [SerializeField] private float jumpForce = 10.0f;
     [SerializeField] private float flapForce = 5.0f;
     [SerializeField] private float maxVelocity = 30f;
-    private float flyFlapTime = 2f;
     [SerializeField] private float initialFlyFlapTime = 2f;
     [SerializeField] private bool _canJump;
     [SerializeField] private bool _canFly;
     [SerializeField] private bool _canRun;
- 
+    private float flyFlapTime = 2f;
     private float horizontalInput;
     private float verticalInput;
     private Vector3 movedirection;
     public Rigidbody objRb;
-    [SerializeField] private bool _active;
-
+    
     private Vector3 connectionOffset;
     private bool _connectedToOther;
     private GameObject connectedObj;
@@ -37,11 +35,7 @@ public class PlayerMovment : MonoBehaviour
             transform.position = connectedObj.transform.position + connectionOffset;
 
         }
-        if (!_active )
-        {
-            movedirection = Vector3.zero;
-            return;
-        }
+    
         
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
@@ -82,6 +76,12 @@ public class PlayerMovment : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Run();
+        objRb.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    private void Run()
+    {
         speed = _canRun ? 20f : 2f;
         maxVelocity = _canRun ? 10f : 5f;
         objRb.AddForce(movedirection * speed);
@@ -89,21 +89,8 @@ public class PlayerMovment : MonoBehaviour
         if (movedirection != Vector3.zero) return;
         var newVel = new Vector3(0, objRb.velocity.y, 0);
         objRb.velocity = newVel;
-        objRb.rotation = Quaternion.Euler(0, 0, 0);
     }
-    
-    public void setActive (bool active)
-    {
-        _active = active;
-    }
-    
-    public void SetConnection (bool connected, Transform parent)
-    {
-        _connectedToOther = connected;
-        transform.parent = parent;
-        connectedObj = parent.gameObject;
-        connectionOffset = transform.position - connectedObj.transform.position;
-    }
+
 
     #region Setters and Getters
 
