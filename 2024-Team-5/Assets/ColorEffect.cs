f9c7db4a9bad1de2c7fced3cdfeed0c5b44e039b
Gal <galgulz96@gmail.com>
2024-01-16 21:11:49 +0200

    color changes gradually

--- 2024-Team-5/Assets/ColorEffect.cs
+++ 2024-Team-5/Assets/ColorEffect.cs
@@ -7,19 +7,42 @@ using UnityEngine.Rendering.Universal;
 
 public class ColorEffect : MonoBehaviour
 {
-     private ColorAdjustments colorAdjustments;
+    private ColorAdjustments _colorAdjustments;
     [SerializeField] private Volume volume;
+    public float startSaturation = -100f;
+    public float endSaturation = 0f;
 
     private void Start()
     {
-        volume.profile.TryGet<UnityEngine.Rendering.Universal.ColorAdjustments>(out colorAdjustments);
+        volume.profile.TryGet<UnityEngine.Rendering.Universal.ColorAdjustments>(out _colorAdjustments);
+        _colorAdjustments.saturation.value = startSaturation;
     }
 
-    public void ColorfulEffect()
+    // public void ColorfulEffect()
+    // {
+    //     colorAdjustments.saturation.value = 0;
+    //     // colorAdjustments.contrast.value = 100;
+    //     // colorAdjustments.hueShift.value = 100;
+    // }
+    
+    public IEnumerator ChangeSaturationOverTime(float duration)
     {
-        // volume.GetComponent<ColorAdjustments>().saturation.value = 100; 
-        colorAdjustments.saturation.value = 100;
-        // colorAdjustments.contrast.value = 100;
-        // colorAdjustments.hueShift.value = 100;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.fixedDeltaTime;
+            
+            float currentSaturation = Mathf.Lerp(startSaturation, endSaturation, elapsedTime / duration);
+
+            // Apply the current saturation value 
+            _colorAdjustments.saturation.value = (int) currentSaturation;
+            Debug.Log("Saturation: " + _colorAdjustments.saturation.value);
+
+            yield return null;
+        }
+        
+        // Ensure that the final saturation is exactly the end value
+        _colorAdjustments.saturation.value = endSaturation;
     }
 }
--- 2024-Team-5/Assets/EyeScript.cs
+++ 2024-Team-5/Assets/EyeScript.cs
@@ -6,12 +6,14 @@ using UnityEngine.Rendering;
 public class EyeScript : MonoBehaviour
 {
     public ColorEffect colorEffect;
+    public float colorChangeDuration = 3f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {
-            Destroy(gameObject);
-            colorEffect.ColorfulEffect();
-        }
+        if (!other.CompareTag("Player")) return;
+        // colorEffect.ColorfulEffect();
+        StartCoroutine(colorEffect.ChangeSaturationOverTime(colorChangeDuration));
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
     }
 }
