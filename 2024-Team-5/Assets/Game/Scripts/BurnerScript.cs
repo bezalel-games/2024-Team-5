15aa36c5d4bac3562e7923fc432902b6add49d5a
suchetzky <tal.suchetzky@mail.huji.ac.il>
2024-01-24 20:56:39 +0200

    updated scene

--- 2024-Team-5/Assets/Game/Scripts/BurnerScript.cs
+++ 2024-Team-5/Assets/Game/Scripts/BurnerScript.cs
@@ -12,10 +12,12 @@ public class HoverScript : MonoBehaviour
     [SerializeField] float maxHoverSpeed = 5f;
     private SpriteRenderer spriteRenderer;
 
+    [SerializeField] float hoverDepth = 5f; // Depth to which the player hovers
+    [SerializeField] float hoverSpeed = 2f; // Speed at which the player returns to Z-axis = 0
+
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        
     }
 
     void Update()
@@ -37,6 +39,13 @@ public class HoverScript : MonoBehaviour
         {
             PerformHover();
         }
+        else
+        {
+            // Gently move back to Z-axis = 0
+            float step = hoverSpeed * Time.fixedDeltaTime;
+            transform.position = Vector3.MoveTowards(transform.position,
+                new Vector3(transform.position.x, transform.position.y, 0f), step);
+        }
     }
 
     void StartHover()
@@ -49,14 +58,14 @@ public class HoverScript : MonoBehaviour
     {
         isHovering = false;
         spriteRenderer.sprite = burnerWithOutFireSprite;
-        
     }
 
     void PerformHover()
     {
         if (playerRigidbody2D.velocity.magnitude < maxHoverSpeed)
         {
-            playerRigidbody2D.AddForce(Vector2.up * hoverForce, ForceMode2D.Force);
+            // Apply hover force in the forward direction (Z-axis)
+            playerRigidbody2D.AddForce(Vector3.forward * hoverForce, ForceMode2D.Force);
         }
     }
 }
