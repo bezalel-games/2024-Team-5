398564650a0e57192a09b4c18fd74ea87f129ff2
suchetzky <tal.suchetzky@mail.huji.ac.il>
2024-02-15 11:54:37 +0200

    order folders

---
+++ 2024-Team-5/Assets/Scripts/caveEntrance.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class caveEntrance : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name != "Mouse") return;
+        Destroy(other.transform.parent.parent.gameObject,2f);
+    }
+}
