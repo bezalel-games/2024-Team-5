67863d50d6d211cb0ab12e4ec7ab0e5511b3bfae
Gal <galgulz96@gmail.com>
2024-02-28 16:01:46 +0200

    trying to fix rendering/collision problem with environment
    should update project version apparently

---
+++ 2024-Team-5/Assets/PlayerLayerOrder.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLayerOrder : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            // change order in layer
+            var playerLayer = gameObject.GetComponent<SpriteRenderer>().sortingOrder;
+            var groundLayer = other.gameObject.GetComponent<SpriteRenderer>().sortingOrder;
+            playerLayer = groundLayer;
+            gameObject.GetComponent<SpriteRenderer>().sortingOrder = playerLayer;
+            Debug.Log("Player layer: " + gameObject.GetComponent<SpriteRenderer>().sortingOrder);
+            Debug.Log("Ground layer: " + groundLayer);
+        }
+    }
+}
--- 2024-Team-5/Assets/Scripts/TreeFlup.cs
+++ 2024-Team-5/Assets/Scripts/TreeFlup.cs
@@ -14,6 +14,7 @@ public class TreeFlup : MonoBehaviour
         {
             animator.SetTrigger("Flup");
         }
+      
     }
     
 }
