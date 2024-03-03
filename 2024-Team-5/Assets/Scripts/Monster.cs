38a236e8e3b76e6b6a0d4d9ff06d74a74fe39dd7
Hadar Meron <meron.hadarr@gmail.com>
2024-03-03 23:43:25 +0200

    radar on flashlight

--- 2024-Team-5/Assets/Scripts/Monster.cs
+++ 2024-Team-5/Assets/Scripts/Monster.cs
@@ -5,11 +5,14 @@ public class Monster : Interactable
 {
     public bool hasFlashlight;
     public GameObject flashlight;
+    public Animator radar;
     private bool _mouthOpen;
     private Animator _anim;
     private static readonly int HasFlashlight = Animator.StringToHash("HasFlashlight");
     private static readonly int In = Animator.StringToHash("OpenClose");
-    
+    private static readonly int StartAntenna = Animator.StringToHash("StartAntenna");
+    private static readonly int StopAntenna = Animator.StringToHash("StopAntenna");
+
     public override void Interact()
     {
         if (!_mouthOpen || !hasFlashlight) return;
@@ -17,6 +20,7 @@ public class Monster : Interactable
         // PickupsManager.Instance.CollectLight();
         hasFlashlight = false;
         _anim.SetBool(HasFlashlight, false);
+        radar.SetTrigger(StopAntenna);
     }
 
     private void Start()
@@ -29,12 +33,20 @@ public class Monster : Interactable
     {
         if (!other.CompareTag("Player")) return;
         _anim.SetBool(In, true);
+        if (hasFlashlight)
+        {
+            radar.SetTrigger(StartAntenna);
+        }
     }
     
     private void OnTriggerExit2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
         _anim.SetBool(In, false);
+        if (hasFlashlight)
+        {
+            radar.SetTrigger(StopAntenna);
+        }
     }
     
     /**
